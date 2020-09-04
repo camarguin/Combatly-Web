e788e2075b320acaf38d78eb5ce8e154891a59ba
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2020-09-04 10:24:47 -0700

    Updates

---
+++ precache-manifest.8c16f5f3c9ab1c467848e039ae837dd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9376a19aeb8623f7d05bbc3acf33df",
+    "url": "/Combatly-Web/index.html"
+  },
+  {
+    "revision": "72c3424826e3962bde40",
+    "url": "/Combatly-Web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a655a8641acbd047dcb",
+    "url": "/Combatly-Web/static/js/2.fd21da26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Combatly-Web/static/js/2.fd21da26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c3424826e3962bde40",
+    "url": "/Combatly-Web/static/js/main.1f7d43a8.chunk.js"
+  },
+  {
+    "revision": "aaadd9c72fed818b020c",
+    "url": "/Combatly-Web/static/js/runtime-main.9a60cdb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Combatly-Web/static/media/logo.5d5d9eef.svg"
+  }
+]);
